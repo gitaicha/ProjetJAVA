7444de4476cf5eb06c271eeb8c22f79a96af70d1
gitaicha <aicha.fbd1@gmail.com>
2015-05-18 00:49:01 +0200

    Creation du projet vide

---
+++ CalculatriceStantard/src/calculatricestantard/CalculatriceStantard.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatricestantard;
+
+/**
+ *
+ * @author Aisha SG Jallow
+ */
+public class CalculatriceStantard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
